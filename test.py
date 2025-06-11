f0e04168c9be71650eda6893bda8870ce745c3c2
Lenubolim <3198065092@qq.com>
2025-06-11 15:09:56 +0800

    test1

--- test.py
+++ test.py
@@ -18,6 +18,7 @@ def funtion(path):
             temp = re.sub("[\[\]a-zA-Z0-9@.()（）？。，,“”：:_\"’'/=\-\\n、!]", ' ', line).replace(" ", "")
             total_cnt += len(temp)
     print("中文总字数：", total_cnt)
+    print('end test')
 
 
 if __name__ == '__main__':
